0b9ec2707baaebd8d9b8290a564721eccdb95556
Jeryl Villasoto <jerylannev@gmail.com>
2023-11-14 00:18:34 -0800

    bg_scaled

--- IAT359_Project/app/src/main/java/com/example/iat359_project/MainActivity.java
+++ IAT359_Project/app/src/main/java/com/example/iat359_project/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.iat359_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,13 +10,11 @@ public class MainActivity extends AppCompatActivity {
 
     int main_bg;
 
-    public static final enum ImageView.ScaleType extends Enum<ImageView.ScaleType>
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView.main_bg(FIT_XY);
     }
 }
