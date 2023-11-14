eabd9c0e94c9a87155e24e048fe6fd6371db1bc2
Jeryl Villasoto <jerylannev@gmail.com>
2023-11-14 00:11:43 -0800

    bg_test2

--- IAT359_Project/app/src/main/java/com/example/iat359_project/MainActivity.java
+++ IAT359_Project/app/src/main/java/com/example/iat359_project/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.iat359_project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     int affection=0;
     int currency=100;
 
+    int main_bg;
+
+    public static final enum ImageView.ScaleType extends Enum<ImageView.ScaleType>
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView.main_bg(FIT_XY);
     }
 }
