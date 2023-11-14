97fd400edba0f85d3002a3ba2d4bea983d7ec3de
Jeryl Villasoto <jerylannev@gmail.com>
2023-11-14 13:25:48 -0800

    added buttons
    linked shop button to shop, linked home button to home

--- IAT359_Project/app/src/main/java/com/example/iat359_project/MainActivity.java
+++ IAT359_Project/app/src/main/java/com/example/iat359_project/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.iat359_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     int affection=0;
     int currency=100;
 
-    int main_bg;
+    //buttons
+    Button shop, home, feed, play;
 
 
     @Override
@@ -16,5 +21,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Nav Buttons
+        shop = (Button)findViewById(R.id.button1);
+        home = (Button)findViewById(R.id.button2);
+        feed = (Button)findViewById(R.id.button3);
+        play = (Button)findViewById(R.id.button4);
+    }
+
+    public void gotoShop(View view) {
+        Intent i = new Intent(this, Shop.class);
+        startActivity(i);
     }
 }
--- IAT359_Project/app/src/main/java/com/example/iat359_project/Shop.java
+++ IAT359_Project/app/src/main/java/com/example/iat359_project/Shop.java
@@ -2,13 +2,30 @@ package com.example.iat359_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Shop extends AppCompatActivity {
+
+    //buttons
+    Button shop, home, feed, play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        //Nav Buttons
+        shop = (Button)findViewById(R.id.button1);
+        home = (Button)findViewById(R.id.button2);
+        feed = (Button)findViewById(R.id.button3);
+        play = (Button)findViewById(R.id.button4);
+    }
+
+    public void goHome(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
