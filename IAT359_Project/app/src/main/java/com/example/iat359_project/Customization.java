b9268aa8ef15c13f3de603d4634a56824c2b9b50
Jeryl Villasoto <jerylannev@gmail.com>
2023-11-14 16:14:14 -0800

    updated shop assets

--- IAT359_Project/app/src/main/java/com/example/iat359_project/Customization.java
+++ IAT359_Project/app/src/main/java/com/example/iat359_project/Customization.java
@@ -14,7 +14,7 @@ public class Customization extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customization);
-        
+
     }
 
     //for shop button
