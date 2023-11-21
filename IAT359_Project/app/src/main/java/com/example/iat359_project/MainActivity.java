6cb7f755317e07501eb326ad844d37e02f32d71d
Jeryl Villasoto <jerylannev@gmail.com>
2023-11-20 18:43:19 -0800

    added GPS + Weather in main

--- IAT359_Project/app/src/main/java/com/example/iat359_project/MainActivity.java
+++ IAT359_Project/app/src/main/java/com/example/iat359_project/MainActivity.java
@@ -12,14 +12,27 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -28,7 +41,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     ImageView toy, food, plate, night;
     TextView petNameView;
-    String petName;
+    String lat, lng, result, url;
     boolean feeding = false;
     boolean playing = false;
 
@@ -49,6 +62,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
         SharedPreferences sharedPref = getSharedPreferences("MyData", Context.MODE_PRIVATE);
         String petName = sharedPref.getString("petName", DEFAULT);
+        checkConnection();
 
         //using the player database to ensure they are wearing clothes from last session
         playerdb = new MyPlayerDatabase(this);
@@ -152,6 +166,96 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
 
         }
+    }
+
+    //GPS + weather
+    public void getWeather(View v) {
+        url = "http://api.geonames.org/findNearByWeatherJSON?lat=";
+
+        Thread myThread = new Thread(new GetWeatherThread());
+        myThread.start();
+
+        try {
+            JSONObject jsonObject = new JSONObject(result);
+            JSONObject weatherObservationItems =
+                    new JSONObject(jsonObject.getString("weatherObservation"));
+
+
+        } catch (Exception e) {
+            Log.d("ReadWeatherJSONDataTask", e.getLocalizedMessage());
+        }
+    }
+
+    private class GetWeatherThread implements Runnable
+    {
+        @Override
+        public void run() {
+            Exception exception = null;
+            try{
+                result = readJSONData(url);
+            }catch(IOException e){
+                exception = e;
+            }
+        }
+    }
+
+    public void checkConnection(){
+        ConnectivityManager connectMgr =
+                (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectMgr.getActiveNetworkInfo();
+        if(networkInfo != null && networkInfo.isConnected()){
+            //fetch data
+
+            String networkType = networkInfo.getTypeName().toString();
+            Toast.makeText(this, "connected to " + networkType, Toast.LENGTH_LONG).show();
+        }
+        else {
+            //display error
+            Toast.makeText(this, "no network connection", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private String readJSONData(String myurl) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 2500;
+
+        URL url = new URL(myurl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        try {
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("tag", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+                conn.disconnect();
+            }
+        }
+    }
+
+    // Reads an InputStream and converts it to a String.
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
     }
 
 }
