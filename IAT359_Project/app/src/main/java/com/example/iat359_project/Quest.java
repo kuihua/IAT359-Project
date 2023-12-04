99522627b68b4286384500ffde3c43a8436b5d72
kuihua <michellelin2235@gmail.com>
2023-12-03 18:52:30 -0800

    quest comments

--- IAT359_Project/app/src/main/java/com/example/iat359_project/Quest.java
+++ IAT359_Project/app/src/main/java/com/example/iat359_project/Quest.java
@@ -1,9 +1,6 @@
 package com.example.iat359_project;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -15,12 +12,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
-
 public class Quest extends AppCompatActivity {
 
     Button playQuest, feedQuest, petQuest;
-    TextView playReward, feedReward, petReward;
+    TextView playDesc, feedDesc, petDesc, playReward, feedReward, petReward;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,20 +28,34 @@ public class Quest extends AppCompatActivity {
         feedQuest = (Button) findViewById(R.id.collectButton2);
         petQuest = (Button) findViewById(R.id.collectButton3);
 
+        //setting quest descriptions
+        playDesc = (TextView) findViewById(R.id.questDesc1);
+        feedDesc= (TextView) findViewById(R.id.questDesc2);
+        petDesc = (TextView) findViewById(R.id.questDesc3);
+
         //setting reward
         playReward = (TextView) findViewById(R.id.reward1);
-        feedReward = (TextView) findViewById(R.id.reward2);
+        feedReward= (TextView) findViewById(R.id.reward2);
         petReward = (TextView) findViewById(R.id.reward3);
 
         SharedPreferences sharedPref = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        //values to check if the user has fulfilled the quest
         boolean pet = sharedPref.getBoolean("pet", false);
         boolean feed = sharedPref.getBoolean("feed", false);
         boolean play = sharedPref.getBoolean("play", false);
 
+        //values to check if the user has already collected the reward
         boolean collected1 = sharedPref.getBoolean("collected1", false);
         boolean collected2 = sharedPref.getBoolean("collected2", false);
         boolean collected3 = sharedPref.getBoolean("collected3", false);
 
+        //setting quest descriptions with the user pet's name
+        String petName = sharedPref.getString("petName", "Yumi");
+        playDesc.setText("Play with "+petName);
+        feedDesc.setText("Feed "+petName);
+        petDesc.setText("Pet "+petName);
+
+        //setting button text depending on the quest and collection status
         if(play && !collected1){
             playQuest.setClickable(true);
             playQuest.setText("Collect");
@@ -88,11 +97,13 @@ public class Quest extends AppCompatActivity {
         Log.d("rewardAmt", ""+rewardAmt);
         SharedPreferences sharedPref = getSharedPreferences("MyData", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
+        //giving the user money for completion, updating coin value
         int currentMoney = sharedPref.getInt("coin", 100);
         int newMoney = currentMoney+rewardAmt;
         editor.putInt("coin", newMoney);
         editor.putBoolean("collected1", true);
         editor.commit();
+        //setting button to collected, disable button
         playQuest.setText("Collected");
         playQuest.setClickable(false);
         Toast.makeText(this, "Gained "+ rewardAmt, Toast.LENGTH_SHORT).show();
@@ -102,11 +113,12 @@ public class Quest extends AppCompatActivity {
         int rewardAmt = Integer.parseInt(feedReward.getText().toString().replace("Reward: ", ""));
         SharedPreferences sharedPref = getSharedPreferences("MyData", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
+        //giving the user money for completion, updating coin value
         int currentMoney = sharedPref.getInt("coin", 100);
         int newMoney = currentMoney+rewardAmt;
         editor.putInt("coin", newMoney);
         editor.putBoolean("collected2", true);
-        editor.commit();
+        //setting button to collected, disable button
         feedQuest.setText("Collected");
         feedQuest.setClickable(false);
         Toast.makeText(this, "Gained "+ rewardAmt, Toast.LENGTH_SHORT).show();
@@ -116,11 +128,13 @@ public class Quest extends AppCompatActivity {
         int rewardAmt = Integer.parseInt(petReward.getText().toString().replace("Reward: ", ""));
         SharedPreferences sharedPref = getSharedPreferences("MyData", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
+        //giving the user money for completion, updating coin value
         int currentMoney = sharedPref.getInt("coin", 100);
         int newMoney = currentMoney+rewardAmt;
         editor.putInt("coin", newMoney);
         editor.putBoolean("collected3", true);
         editor.commit();
+        //setting button to collected, disable button
         petQuest.setText("Collected");
         petQuest.setClickable(false);
         Toast.makeText(this, "Gained "+ rewardAmt, Toast.LENGTH_SHORT).show();
@@ -137,7 +151,7 @@ public class Quest extends AppCompatActivity {
         public void run() {
             //check every 2s
             SystemClock.sleep(2000);
-            // if all quests are completed, refresh them
+            // if all quests are completed, refresh them (reset values to default)
             if (checkQuestRefresh()) {
                 SharedPreferences sharedPref = getSharedPreferences("MyData", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPref.edit();
@@ -150,6 +164,7 @@ public class Quest extends AppCompatActivity {
                 editor.putBoolean("collected3", false);
                 editor.commit();
 
+                //update button text
                 runOnUiThread(new Runnable(){
                     @Override
                     public void run() {
@@ -157,9 +172,9 @@ public class Quest extends AppCompatActivity {
                         playQuest.setText("In\nProgress");
                         petQuest.setText("In\nProgress");
                         feedQuest.setText("In\nProgress");
-                    }
+                    } // end of UI run
                 }); // end of ui thread
-            } // end of if
+            } // end of quest status if statement
         } // end of run
     } // end of questThread
 
@@ -177,7 +192,7 @@ public class Quest extends AppCompatActivity {
             }
         }
         return false;
-    }
+    } // end of checkQuestRefresh
 
     //shop button
     public void gotoShop(View view) {
