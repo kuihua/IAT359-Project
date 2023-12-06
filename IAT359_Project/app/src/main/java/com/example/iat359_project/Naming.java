77f3c494b2c8402313343ecc1c98200474ef0176
kuihua <michellelin2235@gmail.com>
2023-12-05 17:32:53 -0800

    naming comments

--- IAT359_Project/app/src/main/java/com/example/iat359_project/Naming.java
+++ IAT359_Project/app/src/main/java/com/example/iat359_project/Naming.java
@@ -12,11 +12,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+// Naming activity, the first activity the user sees as a first time user
+// or if the user wants to rename their pet, this is the activity they see
 public class Naming extends AppCompatActivity {
 
     EditText petNameEdit;
     boolean rename;
-    boolean firstTime, isPlaying;
+    boolean firstTime;
     MyDatabase db;
     MediaPlayer mp;
 
@@ -31,12 +33,13 @@ public class Naming extends AppCompatActivity {
         firstTime = sharedPrefs.getBoolean("firstTime", true);
         rename = sharedPrefs.getBoolean("rename", false);
 
+        // if the user is not a first time user or renaming their creature, bring them to the main activity
         if (!firstTime && !rename) {
             Intent i = new Intent(this, MainActivity.class);
             startActivity(i);
         }
 
-        //when user is a first time user, add all data to the db shop table
+        //when user is a first time user, add all initial data to the db shop table
         if(firstTime){
             db = new MyDatabase(this);
             db.insertShopData("Bowtie", "Neck", "35", "bowtie_icon.png");
@@ -61,6 +64,7 @@ public class Naming extends AppCompatActivity {
 
     }//end of onCreate
 
+    // button to confirm pet name and brings user to the main activity
     public void name (View view){
         //sfx for tap
         mp = MediaPlayer.create(this, R.raw.tap);
@@ -68,11 +72,11 @@ public class Naming extends AppCompatActivity {
         //save pet's name to shared preferences, player is not a first time user anymore
         SharedPreferences sharedPrefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPrefs.edit();
-        //remembering pet name
+        // saving the inputted text from the edit text into shared preferences
         editor.putString("petName", petNameEdit.getText().toString());
-        //set rename false
+        // set rename false, the player is not renaming the creature
         editor.putBoolean("rename", false);
-        //remembering if its the first time accessing this app
+        // remembering the user isn't a first time user anymore
         editor.putBoolean("firstTime", false);
         //save data to shared prefs
         editor.commit();
@@ -81,8 +85,6 @@ public class Naming extends AppCompatActivity {
         Toast.makeText(this, "Welcome home "+petNameEdit.getText().toString()+"!", Toast.LENGTH_LONG).show();
         Intent intent= new Intent(this, MainActivity.class);
         startActivity(intent);
-    }//end of naming the pet
-
-
-
+    }//end of naming the pet button
+    
 }//end of activity
